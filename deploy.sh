bd4ab1e65100a23be733fcd17cc6e7c7a1414e8a
cnlinge <liyingjie06@meicai.cn>
2020-10-31 19:55:03 +0800

    publish

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,8 @@ set -e
 npm run docs:build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+# cd docs/.vuepress/dist
+cd public
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -17,9 +18,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:cnlinge/cnlinge.github.io.git master
+# git push -f git@github.com:cnlinge/cnlinge.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:cnlinge/fedoc.git master:gh-pages
 
 cd -
