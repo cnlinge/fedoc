0b5bfa6e8177521902db6005ed2f4b0e0544bbcd
liyingjie06 <liyingjie06@meicai.cn>
2020-11-12 10:55:53 +0800

    测试部署

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'Hello VuePress1111',
   description: 'Just playing around',
-  // base: '/public/',
+  base: '/fedoc/',
   // dest: 'public',
   themeConfig: {
     displayAllHeaders: true, // 默认值：false
